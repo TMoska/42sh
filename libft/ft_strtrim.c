90739a8c86b2a3aa64175511c79b4401e2578834
Tomas Moska <tomas.moska@gmail.com>
2017-03-20 17:06:47 +0100

    check for only space words

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: tmoska <tmoska@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/18 08:03:49 by tmoska            #+#    #+#             */
-/*   Updated: 2017/02/22 21:08:44 by tmoska           ###   ########.fr       */
+/*   Updated: 2017/03/20 17:04:42 by tmoska           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@
 char	*ft_strtrim(char const *s)
 {
 	char	*res;
-	size_t	beg;
-	size_t	end;
+	int		beg;
+	int		end;
 
 	if (!s)
 		return (0);
 	beg = 0;
 	end = 0;
 	end = ft_strlen(s);
-	while (s[beg] == ' ' || s[beg] == '\n' || s[beg] == '\t')
+	while (s[beg] && (s[beg] == ' ' || s[beg] == '\n' || s[beg] == '\t'))
 		beg++;
-	while (s[end - 1] == ' ' || s[end - 1] == '\n' || s[end - 1] == '\t')
+	while (s[end - 1] && (s[end - 1] == ' ' || s[end - 1] == '\n' || s[end - 1] == '\t'))
 		end--;
-	if ((res = ft_strncpy(ft_strnew(end - beg), &s[beg], end - beg)))
+	if ((end - beg > 0) && (res = ft_strncpy(ft_strnew(end - beg), &s[beg], end - beg)))
 		return (res);
 	return (NULL);
 }
