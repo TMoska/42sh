791e35c9b2585f7ba774e27b353f342c5a512051
TMoska <tomas.moska@gmail.com>
2017-03-22 00:26:57 +0100

    extra safety checks for working with str

--- libft/ft_strndelat.c
+++ libft/ft_strndelat.c
@@ -6,7 +6,7 @@
 /*   By: tmoska <tmoska@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/27 16:01:27 by tmoska            #+#    #+#             */
-/*   Updated: 2017/02/27 17:52:24 by tmoska           ###   ########.fr       */
+/*   Updated: 2017/03/22 00:25:54 by moska            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char	*ft_strndelat(char *str, int index, size_t size)
 {
 	char	*ret;
 
+	if (!str)
+		return (NULL);
 	ret = ft_strnew(ft_strlen(str) - size);
 	ret = ft_strncpy(ret, str, index);
 	ret[index] = *ft_strcpy(&ret[index], &str[index + (int)size]);
