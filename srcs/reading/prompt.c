e4cdf31ad4706c3e0a0b63f970535c83f67811d5
Tomas Moska <tomas.moska@gmail.com>
2017-03-26 00:36:48 +0100

    change prompt looks

--- srcs/reading/prompt.c
+++ srcs/reading/prompt.c
@@ -6,7 +6,7 @@
 /*   By: moska <moska@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/08 18:33:26 by moska             #+#    #+#             */
-/*   Updated: 2017/03/12 23:35:57 by tmoska           ###   ########.fr       */
+/*   Updated: 2017/03/26 00:36:33 by tmoska           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,19 @@
 
 void	print_prompt(t_shell **shell, char *prompt)
 {
+	char	*tmp;
+
 	if (!prompt)
-		prompt = ft_strjoin(get_env_val(shell, "PWD"), " $> ");
+	{
+		tmp = ft_str3join("\033[0;35m", get_env_val(shell, "LOGNAME"),\
+				"\033[0;32m:");
+		prompt = ft_strjoin(tmp, get_env_val(shell, "PWD"));
+		ft_strdel(&tmp);
+	}
 	(*shell)->prompt_len = ft_strlen(prompt);
-	ft_putstr("\033[0;32m");
+	ft_putstr("\033[0;33m[\033[0m\033[0;32m");
 	ft_putstr(prompt);
-	ft_putstr("\033[0m");
+	ft_putstr("\033[0m\033[0;33m]\033[0m ");
 	ft_strdel(&prompt);
 }
 
