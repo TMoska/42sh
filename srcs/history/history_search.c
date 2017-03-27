74e0b6c37d20e9de92c0bd73ef7af551543df570
TMoska <tomas.moska@gmail.com>
2017-03-27 12:11:54 +0200

    progerss

--- srcs/history/history_search.c
+++ srcs/history/history_search.c
@@ -6,7 +6,7 @@
 /*   By: moska <moska@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/26 04:14:25 by moska             #+#    #+#             */
-/*   Updated: 2017/03/26 05:05:43 by moska            ###   ########.fr       */
+/*   Updated: 2017/03/26 15:23:02 by moska            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,26 @@ char	*history_search_first_match(t_shell **shell, char *s)
 	{
 		if (ft_strncmp(h_lst->cmd, s, ft_strlen(s)) == 0)
 			return (h_lst->cmd);
+		h_lst = h_lst->next;
+	}
+	return (NULL);
+}
+
+char	*history_search_first_arg_match(t_shell **shell, char *s)
+{
+	t_h_lst	*h_lst;
+	char	*cmpr;
+	int		i;
+
+	h_lst = (*shell)->history->list;
+	while (h_lst)
+	{
+		i = 0;
+		while (!(h_lst->cmd[i] == ' ' || h_lst->cmd[i] == '\0'))
+			i++;
+		cmpr = &h_lst->cmd[i + 1];
+		if (ft_strncmp(cmpr, s, ft_strlen(s)))
+			return (&h_lst->cmd[i]);
 		h_lst = h_lst->next;
 	}
 	return (NULL);
