553116f81f1816f3c82325a3e6bfebe364a3bc41
Anael DELETANG <adeletan@e2r13p1.42.fr>
2017-03-31 07:27:15 +0200

    Fixed copy and paste

--- srcs/reading/mange_buffer.c
+++ srcs/reading/mange_buffer.c
@@ -6,7 +6,7 @@
 /*   By: moska <moska@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/27 19:32:46 by tmoska            #+#    #+#             */
-/*   Updated: 2017/03/31 05:13:31 by adeletan         ###   ########.fr       */
+/*   Updated: 2017/03/31 07:23:55 by adeletan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ static void	write_buffer(t_shell **shell, char *buffer)
 	b = (*shell)->buff;
 	if (!b && ((*shell)->buff = ft_strdup(buffer)))
 		return ;
-	ret = ((in > 1) ? ft_strndup(b, in - 1) : NULL);
-	ret1 = (((len - in) > 0) ? ft_strdup(&b[in - 1]) : NULL);
+	ret = ((in > 1) ? ft_strndup(b, in - ft_strlen(buffer)) : NULL);
+	ret1 = (((len - in) > 0) ? ft_strdup(&b[in - ft_strlen(buffer)]) : NULL);
 	(*shell)->buff = ft_str3join(ret, buffer, ret1);
 	ft_strdel(&ret);
 	ft_strdel(&ret1);
@@ -52,7 +52,10 @@ void		work_buffer(t_shell **shell, char *buffer)
 	i = ft_strlen(buffer);
 	write_buffer(shell, buffer);
 	(*shell)->term->tc_in -= i;
-	while (i--)
+	while (i)
+	{
 		move_right(shell, NULL, 1);
+		i--;
+	}
 	ft_printbuffer(shell);
 }
