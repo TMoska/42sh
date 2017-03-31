670886686929e27e83e7199b5aeecb26c8b68ec2
Ryota YAOI <ryaoi@e2r13p7.42.fr>
2017-03-31 02:46:00 +0200

    protecting malloc inside ft_lst_create_elem.c

--- libft/ft_lst_create_elem.c
+++ libft/ft_lst_create_elem.c
@@ -6,7 +6,7 @@
 /*   By: moska <moska@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 20:56:57 by tmoska            #+#    #+#             */
-/*   Updated: 2017/01/14 16:50:07 by tmoska           ###   ########.fr       */
+/*   Updated: 2017/03/31 02:45:36 by ryaoi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ t_list		*ft_lst_create_elem(void *data)
 {
 	t_list	*new_list;
 
-	new_list = malloc(sizeof(t_list));
+	if (!(new_list = malloc(sizeof(t_list))))
+		return (NULL);
 	new_list->content = data;
 	new_list->next = NULL;
 	return (new_list);
